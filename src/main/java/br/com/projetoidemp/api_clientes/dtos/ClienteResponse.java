f430d788a835b4209f40e6f98557d79e2a82780f
MarcioGomes-Dev <marciojsg.dev@gmail.com>
2026-06-22 21:58:47 -0300

    Versão aprimorada da API Clientes

--- src/main/java/br/com/projetoidemp/api_clientes/dtos/ClienteResponse.java
+++ src/main/java/br/com/projetoidemp/api_clientes/dtos/ClienteResponse.java
@@ -1,11 +1,13 @@
 package br.com.projetoidemp.api_clientes.dtos;
 
+import br.com.projetoidemp.api_clientes.enums.StatusRelatorio;
+
 public record ClienteResponse(
 
         Integer id,
         String nome,
         String email,
         String telefone,
-        String statusRelatorio
+        StatusRelatorio statusRelatorio
 
 ) {}
